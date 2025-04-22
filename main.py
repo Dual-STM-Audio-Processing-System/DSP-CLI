9ea80f6f24c7affe471e013069b9079f9fb3acf6
RyanT <ryantanyx8@gmail.com>
2025-04-22 22:49:49 +0800

    trust

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import read_serial
 
 def main():
-    pass
-
-
+    choice = -1
+    print("Main Menu")
+    print("-------------------------")
+    print("1: Manual Recording Mode")
+    print("2: Distance Recording Mode")
+    while True:
+        try:
+            choice = int(input("Option: "))
+            if choice == 1 or choice == 2:
+                break
+            else:
+                print("Error invalid option")
+        except ValueError:
+            print("Error: Only numbers are accepted")
+    if choice == 1:
+        'Function to send UART2 signal to processing stm to be in manual recording and duration of recording'
+        print("In manual recording mode") 
+    elif choice == 2:
+        'Function to UART2 signal to processing stm to be in distance recording'
+        print("In distance recording mode")
 
 if __name__ == '__main__':
     main()
